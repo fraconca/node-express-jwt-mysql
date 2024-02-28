fb7fd7555f411f7f15a8cb715a11c23b1b900abc
Wrootz <3857571+fraconca@users.noreply.github.com>
2024-02-27 23:22:58 -0300

    MySQL

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+// Require
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const mysql = require('mysql');
 
+// Inicializar Express
 const app = express();
 
-
+const db = mysql.createConnection({
+    host: 'seu_host',
+    user: 'seu_usuario',
+    password: 'sua_senha',
+    database: 'seu_banco_de_dados'
+});
 
 app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
