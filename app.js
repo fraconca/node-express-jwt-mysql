a29c0ca5ab5ef80c0fe7272440d93a38f6cb739d
Wrootz <3857571+fraconca@users.noreply.github.com>
2024-02-27 23:24:55 -0300

    Middleware

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const mysql = require('mysql');
 
+
 // Inicializar Express
 const app = express();
+
 
 // Inicializar e conectar ao MySQL
 const db = mysql.createConnection({
@@ -14,4 +16,22 @@ const db = mysql.createConnection({
     database: 'seu_banco_de_dados'
 });
 
+
+// Middleware de autenticação
+function authenticateToken(req, res, next) {
+
+    const token = req.headers['authorization'];
+    if (!token) return res.status(401).send('Acesso não autorizado');
+    jwt.verify(token, 'seu_segredo', (err, user) => {
+
+        if (err) return res.status(403).send('Token inválido');
+        req.user = user;
+        next();
+
+    });
+
+};
+
+
+// Listen da porta
 app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
