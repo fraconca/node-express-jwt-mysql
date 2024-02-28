93b1ebdfd6c479d0e558565773852380c61f6d42
Wrootz <3857571+fraconca@users.noreply.github.com>
2024-02-27 23:43:23 -0300

    db.js
    SELECT usando Placeholders

--- db.js
+++ db.js
@@ -20,4 +20,14 @@ connection.query(
 );
 
 
+// Usando Placeholders
+connection.query(
+    'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
+    ['Page', 45],
+    function (err, results) {
+      console.log(results);
+    }
+  );
+
+
 connection.end();
